2c0df0fb0e2958e6028a875ea417126ba059b2da
Aaditya <aadityapuriaaditya@gmail.com>
2024-03-07 01:25:01 +0530

    Configuring server.js

--- server.js
+++ server.js
@@ -48,9 +48,9 @@ app.use("/api/comments", comments);
 app.use("/api/messages", messages);
 
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "client", "build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }

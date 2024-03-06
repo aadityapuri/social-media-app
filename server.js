c95948d3b3ebd03389bcb7ba2c52ed8ec1aee387
Aaditya <aadityapuriaaditya@gmail.com>
2024-03-06 23:18:42 +0530

    Changes for vercel deployment

--- server.js
+++ server.js
@@ -37,7 +37,11 @@ httpServer.listen(process.env.PORT || 4000, () => {
 });
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
 app.use("/api/posts", posts);
 app.use("/api/users", users);
 app.use("/api/comments", comments);
